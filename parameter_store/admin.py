9b71adee391433d76271d7de30b29a66807adf23
Paul Durivage <durivage@google.com>
2024-11-05 17:56:12 -0700

    wip

--- parameter_store/admin.py
+++ parameter_store/admin.py
@@ -2,7 +2,42 @@ from django.apps import apps
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
 
+from .models import Cluster, ClusterIntent, ClusterTag
+
+# class ParameterStoreAdmin(admin.AdminSite):
+#     site_header = 'Parameter Store'
+#     site_title = 'Parameter Store Admin'
+#     index_title = 'Welcome to the Parameter Store Admin'
+#
+#     def has_permission(self, request):
+#         return request.user.is_superuser
+# param_store_admin = ParameterStoreAdmin(name='param_store_admin')
+
+admin.site.site_header = 'Parameter Store'
+admin.site.site_title = 'Parameter Store Admin'
+admin.site.index_title = 'Parameter Store Admin'
+
 app = apps.get_app_config('parameter_store')
+app.verbose_name = 'Edge Parameter Store'
+
+
+class ClusterIntentInline(admin.StackedInline):
+    model = ClusterIntent
+    extra = 1
+
+
+class ClusterTagInline(admin.StackedInline):
+    model = ClusterTag
+    extra = 1
+
+
+class ClusterAdmin(admin.ModelAdmin):
+    inlines = [ClusterTagInline, ClusterIntentInline]
+    list_display = ['name', 'group']
+    sortable_by = ['name', 'group']
+
+
+admin.site.register(Cluster, ClusterAdmin)
 
 for model_name, model in app.models.items():
     try:
--- parameter_store/models.py
+++ parameter_store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=30, null=False)
+    name = models.CharField(max_length=30, unique=True, null=False)
     description = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
@@ -10,16 +10,22 @@ class Group(models.Model):
 
 
 class Cluster(models.Model):
-    name = models.CharField(max_length=30, null=False)
+    name = models.CharField(max_length=30, unique=True, null=False)
     description = models.CharField(max_length=255, null=True, blank=True)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
+    data = models.JSONField(null=True, blank=True, default=dict)
+    tags = models.ManyToManyField(
+        'Tag',
+        through='ClusterTag',
+        related_name='clusters',
+    )
 
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=30, null=False)
+    name = models.CharField(max_length=30, unique=True, null=False)
     description = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
@@ -35,13 +41,13 @@ class ClusterTag(models.Model):
 
 
 class ClusterIntent(models.Model):
-    cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+    cluster = models.OneToOneField(Cluster, on_delete=models.CASCADE)
     zone_id = models.CharField(max_length=30, null=False)
     zone_name = models.CharField(max_length=100, null=True, blank=True)
     machine_project_id = models.CharField(max_length=30, null=False)
     fleet_project_id = models.CharField(max_length=30, null=False)
     location = models.CharField(max_length=30, null=False)
-    node_count = models.IntegerField(null=False)
+    node_count = models.IntegerField(null=False, default=3)
     cluster_ipv4_cidr = models.CharField(max_length=18, null=False)
     services_ipv4_cidr = models.CharField(max_length=18, null=False)
     external_load_balancer_ipv4_address_pools = models.CharField(max_length=18, null=False)
@@ -52,10 +58,14 @@ class ClusterIntent(models.Model):
     git_token_secrets_manager_name = models.CharField(max_length=255, null=False)
     cluster_version = models.CharField(max_length=30, null=False)
     maintenance_window_start = models.TimeField(null=True, blank=True)
-    maintenance_window_end = models.CharField(max_length=30, null=True, blank=True)
+    maintenance_window_end = models.TimeField(null=True, blank=True)
     maintenance_window_recurrence = models.CharField(max_length=128, null=True, blank=True)
     subnet_vlans = models.CharField(max_length=128, null=False)
     recreate_on_delete = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Cluster Intent Data'
+        verbose_name_plural = 'Cluster Intent Data'
 
     def __str__(self):
         return self.cluster.name
@@ -68,28 +78,27 @@ class ClusterFleetLabel(models.Model):
     def __str__(self):
         return f'{self.cluster.name} - {self.label}'
 
-
-class Role(models.Model):
-    member = models.CharField(max_length=100, null=False)
-    create = models.BooleanField(null=False)
-    read = models.BooleanField(null=False)
-    update = models.BooleanField(null=False)
-    delete = models.BooleanField(null=False)
-
-    class Meta:
-        abstract = True
-
-
-class ClusterRole(Role):
-    cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
-
-
-class GroupRole(Role):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-
-
-class GlobalRole(Role):
-    pass
+# class Role(models.Model):
+#     member = models.CharField(max_length=100, null=False)
+#     create = models.BooleanField(null=False)
+#     read = models.BooleanField(null=False)
+#     update = models.BooleanField(null=False)
+#     delete = models.BooleanField(null=False)
+#
+#     class Meta:
+#         abstract = True
+#
+#
+# class ClusterRole(Role):
+#     cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+#
+#
+# class GroupRole(Role):
+#     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+#
+#
+# class GlobalRole(Role):
+#     pass
 
 # class CustomFields(models.Model):
 #     table_id = models.CharField(max_length=50, null=False)
