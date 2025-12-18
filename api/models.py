c34fe1dc354f0a78761b026f81a8a8f7c0edf556
Paul Durivage <durivage@google.com>
2025-12-18 16:13:11 -0600

    refactor: put API permissions model in correct location for Django

---
+++ api/models.py
@@ -0,0 +1,63 @@
+###############################################################################
+# Copyright 2024 Google, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+"""
+Custom Permission Definitions for the Parameter Store API.
+
+This module defines the `CustomAPIPermissions` model, which is used to create
+and manage granular permissions for API access (e.g., creating clusters,
+reading groups) via Django's authentication system.
+"""
+
+from django.db import models
+
+
+class CustomAPIPermissions(models.Model):
+    """
+    Abstract model to define API permissions.
+    This doesn't create a database table but provides a Django-like way to define permissions.
+    """
+
+    class Meta:
+        managed = False
+        default_permissions = ()
+        permissions = [
+            # Global Permissions
+            ("params_api_create_objects", "Can create any object via API"),
+            ("params_api_read_objects", "Can read any object via API"),
+            ("params_api_update_objects", "Can update any object via API"),
+            ("params_api_delete_objects", "Can delete any object via API"),
+            # ChangeSet Permissions
+            ("params_api_create_changeset", "Can create ChangeSet via API"),
+            ("params_api_read_changeset", "Can read ChangeSet via API"),
+            ("params_api_update_changeset", "Can update ChangeSet via API"),
+            ("params_api_delete_changeset", "Can delete ChangeSet via API"),
+            # Group Permissions
+            ("params_api_create_group", "Can create Group via API"),
+            ("params_api_read_group", "Can read Group via API"),
+            ("params_api_update_group", "Can update Group via API"),
+            ("params_api_delete_group", "Can delete Group via API"),
+            # Cluster Permissions
+            ("params_api_create_cluster", "Can create Cluster via API"),
+            ("params_api_read_cluster", "Can read Cluster via API"),
+            ("params_api_update_cluster", "Can update Cluster via API"),
+            ("params_api_delete_cluster", "Can delete Cluster via API"),
+            # Tag Permissions
+            ("params_api_create_tag", "Can create Tag via API"),
+            ("params_api_read_tag", "Can read Tag via API"),
+            ("params_api_update_tag", "Can update Tag via API"),
+            ("params_api_delete_tag", "Can delete Tag via API"),
+        ]
